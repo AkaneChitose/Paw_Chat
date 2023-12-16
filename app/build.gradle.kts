4706e68e3eafd09a056a1988789160a702daeac3
AkaneChitose <katarina23122000@gmail.com>
2023-12-16 16:51:50 +0700

    Project Setup & Basic Layout Design

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.chatapp.pawchat"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.chatapp.pawchat"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    //This one replaces "findViewById" make Binding class contains direct references to all views
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //This is library use for this project:
+
+    //Scalable Size (compatible with any screen size)
+    implementation ("com.intuit.ssp:ssp-android:1.1.0")
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+
+    //Rounded ImageView
+    implementation ("com.makeramen:roundedimageview:2.3.0")
+
+}
